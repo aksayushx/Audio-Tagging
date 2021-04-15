36d0920e91acff1672881ff0143e8db20d2afac5
aksayushx <aksayushx@gmail.com>
2021-04-16 01:34:55 +0530

    Add conv model

--- eda.py
+++ eda.py
@@ -153,11 +153,9 @@ plt.show()
 
 plot_mfccs(mfccs)
 plt.show()
-'''
 
 if len(os.listdir('clean')) == 0:
     for f in tqdm(df.fname):
         signal, rate = librosa.load('wavfiles/' + f, sr=16000)
         mask = envelope(signal, rate, 0.0005)
         wavfile.write(filename='clean/' + f, rate=rate, data=signal[mask])
-'''
--- model.py
+++ model.py
@@ -3,16 +3,33 @@ from scipy.io import wavfile
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-from keras.layers import Conv2D, MaxPool2D, Flatten, LSTM
-from keras.layers import Dropout, Dense, TimeDistributed
-from keras.models import Sequential
-from keras.utils import to_categorical
+from tensorflow.keras.layers import Conv2D, MaxPool2D, Flatten, LSTM
+from tensorflow.keras.layers import Dropout, Dense, TimeDistributed
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.utils import to_categorical
 from sklearn.utils.class_weight import compute_class_weight
 from tqdm import tqdm
 from python_speech_features import mfcc
+import pickle
+from tensorflow.keras.callbacks import ModelCheckpoint
+from cfg import Config
+
+
+def check_data():
+    if os.path.isfile(config.p_path):
+        print("Loading Model")
+        with open(config.p_path, 'rb') as handle:
+            tmp = pickle.load(handle)
+            return tmp
+    else:
+        return None
 
 
 def build_rand_feat():
+    tmp = check_data()
+    if tmp:
+        return tmp.data[0], tmp.data[1]
+
     X = []
     y = []
     _min, _max = float('inf'), -float('inf')
@@ -27,11 +44,13 @@ def build_rand_feat():
                         rate,
                         numcep=config.nfeat,
                         nfilt=config.nfilt,
-                        nfft=config.nfft).T
+                        nfft=config.nfft)
         _min = min(np.amin(X_sample), _min)
         _max = max(np.amax(X_sample), _max)
-        X.append(X_sample if config.mode == 'conv' else X_sample.T)
+        X.append(X_sample)
         y.append(classes.index(label))
+    config.min = _min
+    config.max = _max
     X, y = np.array(X), np.array(y)
     X = (X - _min) / (_max - _min)
     if config.mode == 'conv':
@@ -39,33 +58,43 @@ def build_rand_feat():
     elif config.mode == 'time':
         X = X.reshape(X.shape[0], X.shape[1], X.shape[2])
     y = to_categorical(y, num_classes=10)
+    config.data = (X, y)
+    with open(config.p_path, 'wb') as handle:
+        pickle.dump(config, handle, protocol=2)
+
     return X, y
 
+
 def get_conv_model():
-    model=Sequential()
-    model.add(Conv2D(16,(3,3),activation='relu',strides=(1,1),padding='same',input_shape=input_shape))
-    model.add(Conv2D(32,(3,3),activation='relu',strides=(1,1),padding='same'))
-    model.add(Conv2D(64,(3,3),activation='relu',strides=(1,1),padding='same'))
-    model.add(Conv2D(128,(3,3),activation='relu',strides=(1,1),padding='same'))
-    model.add(MaxPool2D((2,2)))
+    model = Sequential()
+    model.add(
+        Conv2D(16, (3, 3),
+               activation='relu',
+               strides=(1, 1),
+               padding='same',
+               input_shape=input_shape))
+    model.add(
+        Conv2D(32, (3, 3), activation='relu', strides=(1, 1), padding='same'))
+    model.add(
+        Conv2D(64, (3, 3), activation='relu', strides=(1, 1), padding='same'))
+    model.add(
+        Conv2D(128, (3, 3), activation='relu', strides=(1, 1), padding='same'))
+    model.add(MaxPool2D((2, 2)))
     model.add(Dropout(0.5))
     model.add(Flatten())
-    model.add(Dense(128,activation='relu'))
-    model.add(Dense(64,activation='relu'))
-    model.add(Dense(10,activation='softmax'))
+    model.add(Dense(128, activation='relu'))
+    model.add(Dense(64, activation='relu'))
+    model.add(Dense(10, activation='softmax'))
     model.summary()
-    model.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
+    model.compile(loss='categorical_crossentropy',
+                  optimizer='adam',
+                  metrics=['accuracy'])
     return model
 
 
-class Config:
-    def __init__(self, mode='conv', nfilt=26, nfeat=13, nfft=512, rate=16000):
-        self.mode = mode
-        self.nfilt = nfilt
-        self.nfeat = nfeat
-        self.rate = rate
-        self.nfft = nfft
-        self.step = int(rate / 10)
+def get_recurrent_model():
+    #Add later
+    return 0
 
 
 df = pd.read_csv('instruments.csv')
@@ -96,17 +125,33 @@ config = Config(mode='conv')
 
 if config.mode == 'conv':
     X, y = build_rand_feat()
-    y_flat=np.argmax(y,axis=1)
-    input_shape=(X.shape[1],X.shape[2],1)
-    model=get_conv_model()
+    y_flat = np.argmax(y, axis=1)
+    input_shape = (X.shape[1], X.shape[2], 1)
+    model = get_conv_model()
 
 elif config.mode == 'time':
     X, y = build_rand_feat()
-    y_flat=np.argmax(y,axis=1)
-    input_shape=(X.shape[1],X.shape[2])
-    model=get_recurrent_model()
+    y_flat = np.argmax(y, axis=1)
+    input_shape = (X.shape[1], X.shape[2])
+    model = get_recurrent_model()
 
-class_weight=compute_class_weight('balanced',np.unique(y_flat),y_flat)
+class_weight = compute_class_weight('balanced', np.unique(y_flat), y_flat)
 
-model.fit(X,y,epochs=10,batch_size=32,shuffle=True,class_weight=class_weight)
+checkpoint = ModelCheckpoint(config.model_path,
+                             monitor='val_acc',
+                             verbose=1,
+                             mode='max',
+                             save_best_only=True,
+                             save_weights_only=False,
+                             period=1)
 
+model.fit(X,
+          y,
+          epochs=10,
+          batch_size=32,
+          shuffle=True,
+          class_weight=class_weight,
+          validation_split=0.1,
+          callback=[checkpoint])
+
+model.save(config.model_path)
